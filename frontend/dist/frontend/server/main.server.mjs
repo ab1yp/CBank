c046573a32f33210899966601f0dedfb5527fba1
ab1yp <arnyasirpullissery@gmail.com>
2025-02-13 21:33:47 +0300

    Added package.json and dependencies

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4FOORGET.mjs";import"./chunk-ASE5TL62.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JWY32TIZ.mjs";import"./chunk-ASE5TL62.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
